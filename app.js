fa8b5b04ed8eecd139966888bcc213038744ea29
Sowska <losowskisofia@gmail.com>
2023-02-08 21:36:47 -0300

    added login, register and reorganized the containers

--- app.js
+++ app.js
@@ -5,19 +5,24 @@ const publicPath = path.join(__dirname, 'public'); // sirve la carpeta con la ru
 
 const app = express(); // esta linea instancia Express
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath)); //usa mi carpeta public como carpeta de estaticos
 
-app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
-
-});
 
 app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'));
+res.sendFile(path.join(__dirname, './views/home.html'));
     });
 
-/*app.listen(process.env.PORT || 3000, () => { si viene un puerto, lo toma "process.env.PORT"
-    console.log("Levantando un servidor con Express en ", "http://localhost:3000");
-}) */
+app.get('/registro',(req,res) => {
+res.sendFile(path.join(__dirname, './views/register.html'));
+     });
+
+app.get('/login',(req,res) => {
+ res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
+app.listen(port, () => {
+    console.log("Server started succesfully");
+
+});

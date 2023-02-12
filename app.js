05138f88caff6dcf42f0b22e966dfd7da1e4e855
Sowska <losowskisofia@gmail.com>
2023-02-12 01:22:01 -0300

    added login and register (html & css)

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ app.get('/',(req,res) => {
 res.sendFile(path.join(__dirname, './views/home.html'));
     });
 
-app.get('/registro',(req,res) => {
+app.get('/register',(req,res) => {
 res.sendFile(path.join(__dirname, './views/register.html'));
-     });
+    });
 
 app.get('/login',(req,res) => {
- res.sendFile(path.join(__dirname, './views/login.html'));
+res.sendFile(path.join(__dirname, './views/login.html'));
 });
 
 app.listen(port, () => {
